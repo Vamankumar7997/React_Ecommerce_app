9c7af19b5f4f560a6962d7ce85e5762415f4cb61
VAMAN KUMAR TAMARANA <tamaranavamankumar@gmail.com>
2023-03-20 23:50:05 +0530

    Successful Commits

---
+++ my-app/src/pages/contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Contact = () => {
+  return (
+    <div>
+      <h1>Vaman Kumar</h1>
+      <h3>Mobile: 7095114125</h3>
+      <h4>
+        <b>Gmail:</b> tamaranavamankumar@gmail.com
+      </h4>
+    </div>
+  );
+};
